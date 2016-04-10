c4321c65ce20d9d7cec74fbdb15b15d8c404f7f1
Clint Goodman <clintgoodman27@gmail.com>
2016-04-09 21:41:10 -0600

    Seeing a list of evaluations of employees you are over as a manger.  Also adding updated/created meta data

--- app/elements/shokka-evaluation/shokka-evaluation-functions.js
+++ app/elements/shokka-evaluation/shokka-evaluation-functions.js
@@ -28,5 +28,20 @@ var EF = {
       }
       return last[item];
     }, this);
+  },
+  // Copied from: https://gist.github.com/hurjas/2660489
+  timeStamp: function() {
+    var now = new Date();
+    var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+    var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+    var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+    time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+    time[0] = time[0] || 12;
+    for ( var i = 1; i < 3; i++ ) {
+      if ( time[i] < 10 ) {
+        time[i] = "0" + time[i];
+      }
+    }
+    return date.join("/") + " " + time.join(":") + " " + suffix;
   }
 };
